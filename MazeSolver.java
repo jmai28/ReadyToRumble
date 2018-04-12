bf7675778ab99feeb5bc434c8f67e2d2dd9d86cc
jmai28 <34246864+jmai28@users.noreply.github.com>
2018-04-11 22:41:25 -0400

    Add files via upload

---
+++ MazeSolver.java
@@ -0,0 +1,71 @@
+public class MazeSolver {
+
+    public Maze maze;
+
+    // public static void main(String[] commandLine)
+    // 	throws java.io.FileNotFoundException{
+	
+    // 	Maze maze = new Maze( commandLine[0]
+    // 			      , Integer.parseInt( commandLine[1])
+    // 			      , Integer.parseInt( commandLine[2])
+    // 			      );
+    // }
+    
+    public boolean ifSolvable(){
+	// Snapshot
+	// Not sure when we use snapshot, so i'll place it here for now
+	Maze snapshot = new Maze(maze);
+	
+	// Base Case
+	if (maze.explorerIsOnA()== 0){
+	    return true;
+	}
+	else if (maze.explorerIsOnA() != 2){
+	    return false;
+	}
+	else{
+	    maze.dropA(1);
+	    
+	    maze.go(Maze.SOUTH);
+	    if (maze.explorerIsOnA() == 2){
+		ifSolvable();
+	    }
+	    maze.go(Maze.NORTH);
+
+	    maze.go(Maze.NORTH);
+	    if (maze.explorerIsOnA() == 2){
+		ifSolvable();
+	    }
+	    maze.go(Maze.SOUTH);
+
+	    maze.go(Maze.WEST);
+	    if (maze.explorerIsOnA() == 2){
+		ifSolvable();
+	    }
+	    maze.go(Maze.EAST);
+
+	    maze.go(Maze.EAST);
+	    if (maze.explorerIsOnA() == 2){
+		ifSolvable();
+	    }
+	    maze.go(Maze.WEST);
+
+	    
+	
+	}
+	return false;
+    }
+
+    public MazeSolver( String mazeToSolve, int rank, int file)
+	throws java.io.FileNotFoundException{
+	Maze copy = new Maze( mazeToSolve, rank, file);
+	// The Recursive Abstraction will be added in the forseeable future
+	
+    }
+
+    public static void main(String[] args){
+    }
+
+
+    
+}
